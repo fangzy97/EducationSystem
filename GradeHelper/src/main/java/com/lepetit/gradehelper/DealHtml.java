270e4e45ad42e35f49966248ba38c2f37762f638
Mr.Winds <1299012718@qq.com>
2018-11-16 18:39:16 +0800

    Fix the issue of the score in Chinese

--- GradeHelper/src/main/java/com/lepetit/gradehelper/DealHtml.java
+++ GradeHelper/src/main/java/com/lepetit/gradehelper/DealHtml.java
@@ -8,6 +8,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.regex.Pattern;
+
 public class DealHtml {
     private static DealHtml instance;
 
@@ -30,7 +32,7 @@ public class DealHtml {
 
 			String year = tds.get(1).ownText();
 			String course = tds.get(3).ownText();
-			String score = tds.get(4).select("a").text();
+			String score = getScore(tds.get(4).select("a").text());
 			String credit = tds.get(5).ownText();
 			EventBus.getDefault().post(new GradeEvent(year, course, score, credit));
 
@@ -39,6 +41,31 @@ public class DealHtml {
 			System.out.println(score);
 			System.out.println(credit);
 		}
+    }
+
+    // 从网页中获取成绩
+    private String getScore(String score) {
+        // 匹配数字的正则表达式
+        String pattern = "[0-9]+";
+        // 查询数字中是否有数字
+        boolean isHaveNumber = Pattern.matches(pattern, score);
+
+        if (isHaveNumber) {
+            return score;
+        }
+        else {
+            // 将中文的成绩转换为数字
+            switch (score) {
+                case "优秀": return "95";
+                case "良好": return "85";
+                case "中等": return "75";
+                case "及格":
+                case "合格": return "65";
+                case "不及格":
+                case "不合格": return "0";
+            }
+        }
+        return "0";
     }
 
     public static void analyze(String html) {
