5ef310f7441468e31c775743a4e27c201f927919
Mr.Winds <1299012718@qq.com>
2019-04-04 20:01:49 +0800

    测试成绩分析

--- GetTimeHelper/src/test/java/com/lepetit/gettimehelper/GetTimeInfoTest.java
+++ GetTimeHelper/src/test/java/com/lepetit/gettimehelper/GetTimeInfoTest.java
@@ -3,12 +3,10 @@ package com.lepetit.gettimehelper;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class GetTimeInfoTest {
 
     @Test
     public void getPastWeek() {
-        Assert.assertEquals(GetTimeInfo.getPastDay("2018-4-2", "2018-5-12"), 40);
+        Assert.assertEquals(GetTimeInfo.getPastDay("2019-4-4", "2019-6-16"), 73);
     }
 }
